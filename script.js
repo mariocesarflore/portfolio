c48629ede76e4a07e318358e0fa4fe6d4f975052
Mario César Flore de Novaes <165535270+mariocesarflore@users.noreply.github.com>
2025-08-31 23:44:26 -0300

    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function enviarWhats(event) {
+    event.preventDefault()
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5511948187037'
+
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`
+    const msgFormatada = encodeURIComponent(texto)
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`
+
+    window.open(url, '_blank')
+}
